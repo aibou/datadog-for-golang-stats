367bf67c58a803c2b79cf96c0d36447b8b34f3f8
aibou <aibou7251@gmail.com>
2015-08-31 19:15:50 +0900

    Update gostats.py

--- gostats.py
+++ gostats.py
@@ -6,8 +6,31 @@ from util import headers
 from checks import AgentCheck
 
 class GoStats(AgentCheck):
+    GAUGES = [
+        'cpu_num',
+        'cgo_call_num',
+        'goroutine_num',
+        'gomaxprocs',
+        'memory_total_alloc',
+        'memory_lookups',
+        'memory_mallocs',
+        'memory_sys',
+        'memory_frees',
+        'memory_stack',
+        'heap_alloc',
+        'heap_sys',
+        'heap_idle',
+        'heap_inuse',
+        'heap_released',
+        'heap_objects',
+        'gc_num',
+        'gc_per_second',
+        'gc_pause_per_second',
+    ]
 
-    SERVICE_CHECK_NAME = 'gostats.is_ok'
+    HISTGRAMS = [
+        'gc_pause'
+    ]
 
     def check(self, instance):
         if 'gostats_url' not in instance:
@@ -22,11 +45,18 @@ class GoStats(AgentCheck):
             r = requests.get(url, headers=headers(self.agentConfig))
             r.raise_for_status()
             status = r.json()
-        
-            for key, value in status.items():
-                self.log.info('%s:%s' % (key, value))
-                self.gauge('gostats.%s' % (key), value)
-                
+
+            for metric in self.GAUGES:
+                if status.get(metric) is None:
+                    continue
+                self.gauge('gostats.%s' % (metric), status.get(metric))
+
+            for metric in self.HISTGRAMS:
+                if status.get(metric) is None:
+                    continue
+                for value in status.get(metric):
+                    self.histogram('gostats.%s' % (metric), value)
+
         except Exception, e:
             self.log.error('error: %s' % str(e))
             raise
